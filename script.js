4981658253aa31d63db369285fcb25b277bef0e2
anisenson <adamjn524@gmail.com>
2023-12-15 11:23:19 -0500

    Added cases

--- script.js
+++ script.js
@@ -9,4 +9,56 @@ function shakeMagic8Ball(){
         alert(`Please enter a valid question!`)
         return      
     }
+    const randomNumber = Math.floor(Math.random() * 8);
+    let answer, image, color;
+
+    switch(randomNumber) {
+          case 0:
+            answer = 'OF COURSE'
+            image = "of-course.jpg"
+            color = "text-#"
+            break;
+          case 1:
+            answer = 'NO'
+            image = "no.jpg"
+            color = "text-#"
+            break;
+          case 2:
+            answer = 'YES'
+            image = "yes.jpg"
+            color = "text-#"
+            break;
+          case 3:
+            answer = 'MAYBE'
+            image = "maybe.jpg"
+            color = "text-#"
+            break;
+          case 4:
+            answer = 'IDK TRY AGAIN'
+            image = "idk.jpg"
+            color = "text-#"
+            break;
+          case 5:
+            answer = 'CANNOT PREDICT NOW'
+            image = "cannotpredictnow.jpg"
+            color = "text-#"
+            break;
+          case 6:
+            answer = 'DO NOT COUNT ON IT'
+            image = "donotcountonit.jpg"
+            color = "text-#"
+            break;
+          case 7:
+            answer = 'MY SOURCES SAY NO'
+            image = "mysourcessayno.jpg"
+            color = "text-#"
+            break;
+    }
+    
+    
+    
+
+
+
+
 }
